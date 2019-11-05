8504d6837bd9803345de0b553ec32859a951c046
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-11-05 11:01:32 +0100

    Add pieces for interim data import; plot skeleton code

--- data.py
+++ data.py
@@ -1,9 +1,16 @@
 """Retrieve data from the AR6 WGIII Scenarios Database hosted by IIASA."""
 import json
+from pathlib import Path
 
 import pandas as pd
 
 from iiasa_se_client import AuthClient
+
+
+LOCAL_DATA = {
+    'ADVANCE':  'advance_compare_20171018-134445.csv',
+    'AR5': 'ar5_public_version102_compare_compare_20150629-130000.csv',
+    }
 
 
 config = json.load(open('config.json'))
@@ -22,8 +29,20 @@ def get_client():
     return client
 
 
-def get_data(drop=('meta', 'runId', 'time'), **filters):
+def _filter(df, filters):
+    """Helper to filter CSV data."""
+    return df[df.isin(filters)[list(filters.keys())].all(axis=1)]
+
+
+def get_data(drop=('meta', 'runId', 'time'), source='AR6', **filters):
     """Retrieve and return data as a pandas.DataFrame.
+
+    Parameters
+    ----------
+    source : 'AR6' or 'ADVANCE' or 'AR5'
+        Data to load. ADVANCE and AR5 are from local files; see README.
+    drop : list of str
+        Columns to drop when loading from AR6 web API.
 
     Other parameters
     ----------------
@@ -32,12 +51,26 @@ def get_data(drop=('meta', 'runId', 'time'), **filters):
     variables : list of str
         Names of variables to retrieve.
     """
-    get_client()
+    if source in LOCAL_DATA:
+        result = pd.read_csv(Path('data', LOCAL_DATA[source])) \
+                   .rename(columns=lambda c: c.lower()) \
+                   .pipe(_filter, filters) \
+                   .melt(id_vars=['model', 'scenario', 'region', 'variable',
+                                  'unit'],
+                         var_name='year') \
+                   .dropna(subset=['value'])
+        return result
+    else:
+        assert source == 'AR6'
 
-    # Retrieve all data for some runs
-    result = pd.DataFrame.from_dict(client.runs_bulk_ts(**filters))
+        # Get data from the AR6 web API
 
-    if len(result):
-        result.drop(list(drop), axis=1, inplace=True)
+        get_client()
 
-    return result
+        # Retrieve all data for some runs
+        result = pd.DataFrame.from_dict(client.runs_bulk_ts(**filters))
+
+        if len(result):
+            result.drop(list(drop), axis=1, inplace=True)
+
+        return result
--- figures.py
+++ figures.py
@@ -1,13 +1,61 @@
 import logging
+from pathlib import Path
 
 import click
 import pandas as pd
+import plotnine as p9
 
 from data import get_client, get_data
 
 
 log = logging.getLogger()
 
+
+# Individual figures
+
+FIG1_STATIC = [
+    # Ranges of data as vertical bars
+    p9.geom_linerange(p9.aes(ymin='min', ymax='max'), size=4, color='#999999'),
+    p9.geom_linerange(p9.aes(ymin='25%', ymax='75%', color='model'), size=4),
+
+    # Median
+    p9.geom_point(p9.aes(y='50%'), color='black', shape='_', size=3.5),
+
+    # Counts
+    p9.geom_text(p9.aes(label='count'), y=0, size=5),
+
+    # Axis labels
+    p9.labs(x='', y='Transport CO₂ emissions [Mt/y]', color='Model'),
+    p9.theme(axis_text_x=p9.element_text(rotation=90)),
+
+    p9.theme(plot_background=p9.element_rect(alpha=0)),
+]
+
+
+def fig_1():
+    source = 'ADVANCE'
+    data = get_data(source=source, variable=['Transport|CO2|All'],
+                    region=['World'])
+
+    years = ['2020', '2030', '2050', '2100']
+    data = data[data['year'].isin(years)]
+
+    data = data.groupby(['model', 'year']) \
+               .describe()['value'] \
+               .reset_index() \
+               .astype({'count': int})
+    print(data)
+
+    plot = (
+        p9.ggplot(p9.aes(x='model'), data)
+        + p9.facet_wrap('year', ncol=len(years))
+        + p9.ggtitle(f'Figure 1 ({source} database)')
+        + FIG1_STATIC
+        )
+    plot.save(Path('output', '1.pdf'))
+
+
+# Utility methods
 
 def get_references():
     """Retrieve reference files listed in ref/urls.txt to ref/."""
@@ -48,7 +96,7 @@ def refs():
 @cli.command()
 def plot():
     """Generate all plots."""
-    pass
+    fig_1()
 
 
 @cli.command()
