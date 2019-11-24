67a0917672b24f7dc4e8d31b5d60cc49e1639e06
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-11-24 22:34:05 +0100

    Organize figures.py, sketch fig_4

--- figures.py
+++ figures.py
@@ -27,6 +27,8 @@ import yaml
 from data import (
     apply_plot_meta,
     compute_descriptives,
+    compute_ratio,
+    compute_shares,
     get_data,
     restore_dims,
 )
@@ -67,7 +69,77 @@ SCALE_CAT = pd.DataFrame([
     ], columns=['limit', 'fill', 'label'])
 
 
-# Components for individual figures
+def figure(sources=('AR6', 'iTEM MIP2'), **filters):
+    """Decorator to handle common plot tasks.
+
+    Example:
+
+      @figure()
+      def fig_N(iam_data, item_data, sources)
+          # Generate plot...
+          plot.units = 'kg'
+          return plot
+
+      fig_N(options)
+
+    A method (here 'fig_N') wrapped with this decorator:
+
+    - Receives 2 arguments, *iam_data* and *item_data*, with pre-loaded data
+      for the variables under 'fig_N' in figures.yaml. See inline comments in
+      that file.
+    - Receives an argument *sources*, a 2-tuple indicating the original data
+      sources for IAM and sectoral models.
+    - Must return a plotnine.ggplot object with a 'units' attribute.
+
+    The decorated method is then called with a *different* signature, taking
+    only one argument: an optional dict of *options*. These include:
+
+    - 'load_only': if True, then the plot is not written to file. Otherwise,
+      the returned plot object is saved to 'fig_N.pdf'.
+
+    """
+    # Function that decorates the method
+    def figure_decorator(func):
+        # Information about the figure
+        fig_id = func.__name__
+        fig_info = INFO[fig_id]
+        var_names = fig_info['variables']
+
+        # Wrapped method with new signature
+        def wrapped(options={}):
+            # Log output
+            print('-------')
+            log.info(f'{fig_id}')
+
+            # Load IAM and iTEM data
+            iam_data = get_data(
+                source=sources[0],
+                variable=var_names, year=YEARS, **filters) \
+                .pipe(restore_dims, fig_info.get('restore dims', None))
+            item_data = get_data(
+                source=sources[1], conform_to=sources[0],
+                variable=var_names, year=YEARS, **filters)
+
+            # Generate the plot
+            plot = func(
+                iam_data=iam_data,
+                item_data=item_data,
+                sources=sources)
+
+            if plot:
+                # Add a title
+                plot += ggtitle(f"{fig_info['short title']} [{plot.units}] "
+                                f"({fig_id}/{'/'.join(sources)})")
+
+            # Save to file by default
+            if plot and not options.get('load_only', False):
+                args = dict(verbose=False, width=190, height=100, units='mm')
+                plot.save(OUTPUT_PATH / f'{fig_id}.pdf', **args)
+                plot.save(OUTPUT_PATH / f'{fig_id}.png', **args, dpi=300)
+
+        return wrapped
+    return figure_decorator
+
 
 FIG1_STATIC = [
     # Aesthetic mappings
@@ -114,70 +186,6 @@ FIG1_STATIC = [
 ]
 
 
-def figure(sources=('AR6', 'iTEM MIP2'), **filters):
-    """Decorator to handle common plot tasks.
-
-    A method named NAME wrapped with this decorator:
-
-    - Receives 2 arguments, *iam_data* and *item_data*, with pre-loaded data
-      given the corresponding variables in figures.yaml.
-    - Receives an argument *sources*, a 2-tuple indicating the original data
-      sources.
-    - Must return a plotnine.ggplot object, which is then saved to NAME.pdf.
-
-    The decorated method can then be called with a different signature, taking
-    only an optional dict of *options*. These include:
-
-    - 'load_only': if True, then the plot is not written to file.
-
-    """
-    # Function that decorates the method
-    def figure_decorator(func):
-        # Information about the figure
-        fig_id = func.__name__
-        fig_info = INFO[fig_id]
-        var_names = fig_info['variables']
-
-        # Wrapped method with new signature
-        def wrapped(options={}):
-            # Log output
-            print('-------')
-            log.info(f'{fig_id}')
-
-            # Load IAM and iTEM data
-            iam_data = get_data(
-                source=sources[0],
-                variable=var_names,
-                year=YEARS,
-                **filters)
-            item_data = get_data(
-                source=sources[1],
-                conform_to=sources[0],
-                variable=var_names,
-                year=YEARS,
-                **filters)
-
-            # Generate the plot
-            plot = func(
-                iam_data=iam_data,
-                item_data=item_data,
-                sources=sources)
-
-            if plot:
-                # Add a title
-                plot += ggtitle(f"{fig_info['short title']} [{plot.units}] "
-                                f"({fig_id}/{'/'.join(sources)})")
-
-            # Save to file by default
-            if plot and not options.get('load_only', False):
-                args = dict(verbose=False, width=190, height=100, units='mm')
-                plot.save(OUTPUT_PATH / f'{fig_id}.pdf', **args)
-                plot.save(OUTPUT_PATH / f'{fig_id}.png', **args, dpi=300)
-
-        return wrapped
-    return figure_decorator
-
-
 @figure(region=['World'])
 def fig_1(iam_data, item_data, sources):
     # TODO handle optional normalization
@@ -213,8 +221,6 @@ def fig_1(iam_data, item_data, sources):
 @figure()
 def fig_2(iam_data, item_data, sources):
     # Restore the 'type' dimension to each data set
-    expr = r'Energy Service\|Transportation\|(?P<type>Freight|Passenger)'
-    iam_data = restore_dims(iam_data, expr)
     item_data['type'] = item_data['variable'].replace({'tkm': 'Freight',
                                                        'pkm': 'Passenger'})
 
@@ -245,10 +251,8 @@ def fig_2(iam_data, item_data, sources):
 
 @figure()
 def fig_3(iam_data, item_data, sources):
-    # Restore the 'type' and 'mode' dimensions to the IAM data
-    expr = (r'Energy Service\|Transportation\|(?P<type>Freight|Passenger)'
-            r'(?:\|(?P<mode>.*))?')
-    iam_data = restore_dims(iam_data, expr)
+    iam_data = iam_data \
+        .pipe(compute_shares, 'mode')
 
     # TODO mode shares from individual variables
     plot_data = iam_data.pipe(compute_descriptives) \
@@ -261,13 +265,45 @@ def fig_3(iam_data, item_data, sources):
     return plot
 
 
+FIG4_STATIC = [
+    aes(x='category + year', color='category'),
+
+    facet_wrap('type'),
+
+    # Ranges of data as vertical bars
+    geom_crossbar(aes(ymin='min', y='50%', ymax='max'),
+                  color='black', fill='white', width=None),
+    geom_crossbar(aes(ymin='25%', y='50%', ymax='75%', fill='category'),
+                  color='black', width=None),
+
+    # Labels with group counts
+    geom_text(
+        aes(label='count', y='max', color='category'),
+        format_string='{:.0f}',
+        va='bottom',
+        size=7),
+
+    # Scales
+    scale_x_discrete(limits=SCALE_CAT['limit'], labels=SCALE_CAT['label']),
+    scale_fill_manual(limits=SCALE_CAT['limit'], values=SCALE_CAT['fill']),
+    scale_color_manual(limits=SCALE_CAT['limit'], values=SCALE_CAT['fill']),
+]
+
+
 @figure()
 def fig_4(iam_data, item_data, sources):
-    # TODO compute energy intensity from individual variables
+    # Compute energy intensity
+    plot_data = iam_data \
+        .pipe(compute_ratio, groupby=['type'],
+              num="quantity == 'Final Energy'",
+              denom="quantity == 'Energy Service'")
+
+    # TODO compute carbon intensity of energy
+    # TODO compute energy intensity of sectoral models
     plot_data = iam_data.pipe(compute_descriptives) \
                         .pipe(apply_plot_meta, sources[0])
 
-    plot = ggplot(aes(x='model'), plot_data)
+    plot = ggplot(aes(x='model'), plot_data) + FIG4_STATIC
 
     plot.units = '—'
 
