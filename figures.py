1959ceff4d671ddb811163bec3b9ea18f59596f1
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-11-30 17:41:06 +0100

    Add sectoral data to fig_5

--- figures.py
+++ figures.py
@@ -445,11 +445,11 @@ def fig_4(data, sources, **kwargs):
 
 # Non-dynamic features of fig_5
 SCALE_FUEL = pd.DataFrame([
-    ['Liquids', '#f7a800', ''],
-    # ['Biofuels', '#de4911', ''],
-    ['Gases', '#9e2b18', ''],
-    ['Electricity', '#9fca71', ''],
-    ['Hydrogen', '#59a431', ''],
+    ['Liquids|Oil', '#f7a800', 'Oil'],
+    ['Liquids|Biomass', '#de4911', 'Biofuels'],
+    ['Gases', '#9e2b18', 'Gas'],
+    ['Electricity', '#9fca71', 'Electricity'],
+    ['Hydrogen', '#59a431', 'Hydrogen'],
     ], columns=['limit', 'fill', 'label'])
 
 
@@ -459,29 +459,32 @@ FIG5_STATIC = [
 
     # Aesthetics and scales
     aes(x='category', color='fuel'),
-    scale_x_discrete(limits=SCALE_CAT['limit'].iloc[:-2],
-                     labels=SCALE_CAT['label'].iloc[:-2]),
-    scale_y_continuous(limits=(0, 1.05), breaks=np.linspace(0, 100, 6)),
+    scale_x_discrete(limits=SCALE_CAT['limit'],
+                     labels=SCALE_CAT['label']),
+    scale_y_continuous(limits=(-0.02, 1), breaks=np.linspace(0, 1, 6)),
     scale_color_manual(limits=SCALE_FUEL['limit'],
-                       values=SCALE_FUEL['fill']),
+                       values=SCALE_FUEL['fill'],
+                       labels=SCALE_FUEL['label']),
     scale_fill_manual(limits=SCALE_FUEL['limit'],
-                      values=SCALE_FUEL['fill']),
+                      values=SCALE_FUEL['fill'],
+                      labels=SCALE_FUEL['label']),
 
     # Geoms
     # Like COMMON['ranges'], with fill='fuel', position='dodge' and no width=
     geom_crossbar(
         aes(ymin='min', y='50%', ymax='max', group='fuel'), position='dodge',
-        color='black', fill='white'),
+        color='black', fill='white', width=0.9),
     geom_crossbar(
         aes(ymin='25%', y='50%', ymax='75%', fill='fuel'), position='dodge',
-        color='black'),
+        color='black', width=0.9),
     # Like COMMON['counts'], except color is 'fuel'
     geom_text(
-        aes(label='count', y='max', color='fuel'),
+        aes(label='count', y=-0.01, angle=45, color='fuel'),
         position=position_dodge(width=0.9),
+        # commented: this step is extremely slow
+        # adjust_text=dict(autoalign=True),
         format_string='{:.0f}',
-        va='bottom',
-        size=7),
+        va='top', size=3),
 
     # Axis labels
     labs(x='', y='', fill='Energy carrier'),
@@ -501,25 +504,49 @@ FIG5_STATIC = [
 
 @figure(region=['World'])
 def fig_5(data, sources, **kwargs):
+    # TODO reorder colours from Oil -> Hydrogen per AR6
+
     # Compute fuel shares by type for IAM scenarios
     data['iam'] = data['iam'] \
+        .pipe(compute_shares, on='fuel') \
+        .assign(variable='Fuel share')
+    # Compute fuel shares for sectoral scenarios
+    # - Modify labels to match IAM format
+    data['item'] = data['item'] \
+        .replace({'fuel': {
+            'All': None,
+            'Biomass Liquids': 'Liquids|Biomass',
+            'Fossil Liquids': 'Liquids|Oil',
+            }}) \
         .pipe(compute_shares, on='fuel') \
         .assign(variable='Fuel share')
 
     # Discard 2020 data
     data['iam'] = data['iam'][data['iam'].year != 2020]
+    data['item'] = data['item'][data['item'].year != 2020]
 
     # Plot descriptives
     data['plot'] = data['iam'].pipe(compute_descriptives, groupby=['fuel'])
+    # Omit supercategories ('category+1') from iTEM descriptives
+    data['plot-item'] = data['item'] \
+        .drop('category+1', axis=1) \
+        .pipe(compute_descriptives, groupby=['fuel'])
 
-    # Add a column for faceting on two variables
-    data['plot']['facet'] = data['plot']['year'].astype('str') + ' ' \
-        + data['plot']['category']
+    plot = (
+        ggplot(data=data['plot']) + FIG5_STATIC +
 
-    # TODO compute fuel shares for sectoral scenarios
+        # Points and bar for sectoral models
+        geom_crossbar(
+            aes(ymin='min', y='50%', ymax='max', fill='fuel'),
+            data['plot-item'],
+            position='dodge',
+            color='black', fatten=0, width=0.9)
+        + geom_point(
+            aes(y='value', group='fuel'), data['item'],
+            position=position_dodge(width=0.9),
+            color='black', size=1, shape='x', fill=None)
+    )
 
-    plot = ggplot(data=data['plot']) + FIG5_STATIC
-
-    plot.units = 'share'
+    plot.units = '0̸'
 
     return plot
