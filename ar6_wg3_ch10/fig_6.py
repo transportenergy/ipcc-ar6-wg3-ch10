b0426b7d34b2ae3484c94efce88856a717ee4090
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-10-21 14:58:29 +0200

    fig_6: refactor to plot multiple regions

--- ar6_wg3_ch10/fig_6.py
+++ ar6_wg3_ch10/fig_6.py
@@ -3,6 +3,7 @@ import plotnine as p9
 
 from .data import compute_descriptives, normalize_if, select_indicator_scenarios
 from .common import COMMON, figure
+from .util import groupby_multi
 
 # Non-dynamic features of fig_6
 STATIC = (
@@ -17,10 +18,12 @@ STATIC = (
         # # 1 lines per scenario
         # p9.geom_line(p9.aes(y='value', group='model + scenario + category'),
         #           alpha=0.6),
-        # Variant: 1 band per category
-        p9.geom_ribbon(
-            p9.aes(ymin="5%", ymax="95%", fill="category"), alpha=0.25, color=None
-        ),
+        # 1 band per category
+        # p9.geom_ribbon(
+        #     p9.aes(ymin="5%", ymax="95%", fill="category"), alpha=0.25, color=None
+        # ),
+        p9.geom_line(p9.aes(y="5%", color="category"), alpha=0.5, size=0.5),
+        p9.geom_line(p9.aes(y="95%", color="category"), alpha=0.5, size=0.5),
         p9.geom_line(p9.aes(y="50%", color="category"), alpha=0.5),
         # Axis labels
         p9.labs(x="", y="", color="IAM/sectoral scenarios"),
@@ -63,24 +66,33 @@ def plot(data, sources, normalize, overshoot, **kwargs):
     # Select indicator scenarios
     data["indicator"] = select_indicator_scenarios(data["plot"])
 
-    data["descriptives"] = compute_descriptives(data["plot"], on=["type", "mode"])
-
-    plot = (
-        p9.ggplot(data=data["descriptives"])
-        + STATIC
-        + COMMON["color category"](overshoot)
-        + COMMON["fill category"](overshoot)
-        + p9.geom_line(
-            p9.aes(x="year", y="value"),
-            data["indicator"],
-            color="yellow",
-        )
+    data["descriptives"] = compute_descriptives(
+        data["plot"], on=["type", "mode"], groupby=["region"]
     )
 
+    title = kwargs["title"]
     if normalize:
-        plot.units = "Index, 2020 level = 1.0"
+        title = title.format(units="Index, 2020 level = 1.0")
     else:
         units = data["iam"]["unit"].str.replace("bn", "10⁹")
-        plot.units = "; ".join(units.unique())
+        title = title.format(units="; ".join(units.unique()))
 
-    return plot
+    plots = []
+
+    for group, d in groupby_multi(
+        (data["descriptives"], data["indicator"]), "region"
+    ):
+        plots.append(
+            p9.ggplot(data=d[0])
+            + STATIC
+            + COMMON["color category"](overshoot)
+            + COMMON["fill category"](overshoot)
+            + p9.geom_line(
+                p9.aes(x="year", y="value"),
+                d[1],
+                color="yellow",
+            )
+            + p9.ggtitle(title.format(group=group))
+        )
+
+    return plots
