9ee62fd03da2a1fec8ed58752e4886c8fd8b1daa
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-10-31 15:09:27 +0100

    fig_3, fig_4: convert to class pattern

--- ar6_wg3_ch10/fig_3.py
+++ ar6_wg3_ch10/fig_3.py
@@ -2,7 +2,7 @@ import numpy as np
 import plotnine as p9
 
 from .data import compute_shares
-from .common import COMMON, figure
+from .common import COMMON, Figure
 
 # Non-dynamic features of fig_3
 STATIC = (
@@ -29,52 +29,95 @@ STATIC = (
 )
 
 
-@figure
-def plot(data, sources, **kwargs):
-    # Compute mode shares by type for IAM scenarios
-    data["iam"] = (
-        data["iam"]
-        .pipe(compute_shares, on="mode", groupby=["type"])
-        .assign(variable="Mode share")
+class Fig3(Figure):
+    id = "fig_3"
+    title = "Mode shares of transport activity"
+    caption = """
+      Global passenger (billion p-km/yr) and freight (billion t-km/yr) demand
+      projections, 2020 index, based on IAM for selected stabilization temperatures by
+      2100. Also included are global transport models Ref and Policy scenarios."""
+
+    # Data preparation
+    all_years = True
+    variables = [
+        # "Energy Service|Transportation|Aviation",
+        "Energy Service|Transportation|Freight",
+        "Energy Service|Transportation|Freight|Aviation",
+        "Energy Service|Transportation|Freight|International Shipping",
+        "Energy Service|Transportation|Freight|Navigation",
+        "Energy Service|Transportation|Freight|Other",
+        "Energy Service|Transportation|Freight|Railways",
+        "Energy Service|Transportation|Freight|Road",
+        # "Energy Service|Transportation|Navigation",
+        "Energy Service|Transportation|Passenger",
+        "Energy Service|Transportation|Passenger|Aviation",
+        "Energy Service|Transportation|Passenger|Bicycling and Walking",
+        "Energy Service|Transportation|Passenger|Navigation",
+        "Energy Service|Transportation|Passenger|Other",
+        "Energy Service|Transportation|Passenger|Railways",
+        "Energy Service|Transportation|Passenger|Road",
+        "Energy Service|Transportation|Passenger|Road|2W and 3W",
+        "Energy Service|Transportation|Passenger|Road|Bus",
+        "Energy Service|Transportation|Passenger|Road|LDV",
+    ]
+    # Restore the 'type' and 'mode' dimensions to the IAM data
+    restore_dims = (
+        r"Energy Service\|Transportation\|"
+        r"(?P<type>Freight|Passenger)(?:\|(?P<mode>.*))?"
     )
 
-    # Compute fuel shares for sectoral scenarios
-    # - Modify labels to match IAM format
-    data["item"] = (
-        data["item"]
-        .assign(
-            type=data["item"]["variable"].replace(
-                {"pkm": "Passenger", "tkm": "Freight"}
+    def prepare_data(self, data):
+        # Compute mode shares by type for IAM scenarios
+        data["iam"] = (
+            data["iam"]
+            .pipe(compute_shares, on="mode", groupby=["type"])
+            .assign(variable="Mode share")
+        )
+
+        # Compute fuel shares for sectoral scenarios
+        # - Modify labels to match IAM format
+        data["item"] = (
+            data["item"]
+            .assign(
+                type=data["item"]["variable"].replace(
+                    {"pkm": "Passenger", "tkm": "Freight"}
+                )
+            )
+            .replace(
+                {
+                    "mode": {
+                        "All": None,
+                        "Passenger Rail": "Railways",
+                        "Freight Rail": "Railways",
+                        "2W and 3W": "Road|2-/3W",
+                        "Bus": "Road|Bus",
+                        "HDT": "Road|HDT",
+                    }
+                }
+            )
+            .pipe(compute_shares, on="mode", groupby=["type"])
+            .assign(variable="Mode share")
+        )
+
+        # # Separate the IAM and sectoral modes so they can be coloured differently
+        # for k in data.keys():
+        #     data[k]['mode'] = k + '|' + data[k]['mode']
+
+        self.formatted_title = self.formatted_title.format(units="0̸")
+
+        return data
+
+    def generate(self):
+        yield (
+            p9.ggplot(data=self.data["iam"])
+            + STATIC
+            + p9.geom_line(
+                p9.aes(y="value", group="model + scenario + mode"),
+                self.data["item"],
+                alpha=0.6,
             )
         )
-        .replace(
-            {
-                "mode": {
-                    "All": None,
-                    "Passenger Rail": "Railways",
-                    "Freight Rail": "Railways",
-                    "2W and 3W": "Road|2-/3W",
-                    "Bus": "Road|Bus",
-                    "HDT": "Road|HDT",
-                }
-            }
-        )
-        .pipe(compute_shares, on="mode", groupby=["type"])
-        .assign(variable="Mode share")
-    )
 
-    # # Separate the IAM and sectoral modes so they can be coloured differently
-    # for k in data.keys():
-    #     data[k]['mode'] = k + '|' + data[k]['mode']
 
-    plot = (
-        p9.ggplot(data=data["iam"])
-        + STATIC
-        + p9.geom_line(
-            p9.aes(y="value", group="model + scenario + mode"), data["item"], alpha=0.6
-        )
-    )
-
-    plot.units = "0̸"
-
-    return plot
+def save(options):
+    return Fig3(options).save()
--- ar6_wg3_ch10/fig_4.py
+++ ar6_wg3_ch10/fig_4.py
@@ -1,7 +1,7 @@
 import plotnine as p9
 
 from .data import compute_descriptives, compute_ratio
-from .common import COMMON, figure
+from .common import COMMON, Figure
 
 # Non-dynamic features of fig_4
 STATIC = (
@@ -23,55 +23,75 @@ STATIC = (
 )
 
 
-@figure
-def plot(data, sources, overshoot, **kwargs):
-    # Compute energy intensity for IAM scenarios
-    data["iam"] = (
-        data["iam"]
-        .pipe(
+class Fig4(Figure):
+    id = "fig_4"
+    title = "Energy intensity of transport"
+
+    # Data preparation
+    variables = [
+        "Energy Service|Transportation|Freight",
+        "Energy Service|Transportation|Passenger",
+        "Final Energy|Transportation|Freight",
+        "Final Energy|Transportation|Passenger",
+    ]
+    restore_dims = r"(?P<quantity>[^\|]*)\|Transportation(?:\|(?P<type>.*))?"
+
+    # Plotting
+    aspect_ratio = 1.33
+
+    def prepare_data(self, data):
+        # Compute energy intensity for IAM scenarios
+        data["iam"] = (
+            data["iam"]
+            .pipe(
+                compute_ratio,
+                groupby=["type"],
+                num="quantity == 'Final Energy'",
+                denom="quantity == 'Energy Service'",
+            )
+            .assign(variable="Energy intensity of transport")
+        )
+
+        data["plot"] = data["iam"].pipe(compute_descriptives, groupby=["type"])
+
+        # Compute energy intensity for sectoral scenarios
+        data["item"] = data["item"].pipe(
             compute_ratio,
             groupby=["type"],
             num="quantity == 'Final Energy'",
             denom="quantity == 'Energy Service'",
         )
-        .assign(variable="Energy intensity of transport")
-    )
+        data["plot-item"] = data["item"].pipe(compute_descriptives, groupby=["type"])
 
-    units = sorted(map(str, data["iam"]["unit"].unique()))
+        # TODO compute carbon intensity of energy
 
-    data["plot"] = data["iam"].pipe(compute_descriptives, groupby=["type"])
+        units = sorted(map(str, data["iam"]["unit"].unique()))
+        self.formatted_title = self.formatted_title.format(units=units)
 
-    # Compute energy intensity for sectoral scenarios
-    data["item"] = data["item"].pipe(
-        compute_ratio,
-        groupby=["type"],
-        num="quantity == 'Final Energy'",
-        denom="quantity == 'Energy Service'",
-    )
-    data["plot-item"] = data["item"].pipe(compute_descriptives, groupby=["type"])
+        return data
 
-    # TODO compute carbon intensity of energy
-
-    plot = (
-        p9.ggplot(data=data["plot"])
-        + STATIC
-        # Aesthetics and scales
-        + COMMON["x category"](overshoot)
-        + COMMON["color category"](overshoot)
-        + COMMON["fill category"](overshoot)
-        # Points and bar for sectoral models
-        + p9.geom_crossbar(
-            p9.aes(ymin="min", y="50%", ymax="max", fill="category"),
-            data["plot-item"],
-            color="black",
-            fatten=0,
-            width=None,
+    def generate(self):
+        yield (
+            p9.ggplot(data=self.data["plot"])
+            + STATIC
+            # Aesthetics and scales
+            + COMMON["x category"](self.overshoot)
+            + COMMON["color category"](self.overshoot)
+            + COMMON["fill category"](self.overshoot)
+            # Points and bar for sectoral models
+            + p9.geom_crossbar(
+                p9.aes(ymin="min", y="50%", ymax="max", fill="category"),
+                self.data["plot-item"],
+                color="black",
+                fatten=0,
+                width=None,
+            )
+            + p9.geom_point(
+                p9.aes(y="value"),
+                self.data["item"],
+                color="black",
+                size=1,
+                shape="x",
+                fill=None
+            )
         )
-        + p9.geom_point(
-            p9.aes(y="value"), data["item"], color="black", size=1, shape="x", fill=None
-        )
-    )
-
-    plot.units = units
-
-    return plot
