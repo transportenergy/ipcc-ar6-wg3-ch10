7bf1db14b32b2aff24dcc2f38af90226089b8509
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-11-10 17:08:38 +0100

    Add current.sh

---
+++ current.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+python3 ar6_wg3_ch10 plot --ar6-data="world" 1
+python3 ar6_wg3_ch10 plot --ar6-data="R5" 1
+python3 ar6_wg3_ch10 plot --ar6-data="R10" 1
+python3 ar6_wg3_ch10 plot --ar6-data="country" 1
+
+python3 ar6_wg3_ch10 plot --ar6-data="world" 2
+python3 ar6_wg3_ch10 plot --ar6-data="R5" 2
+python3 ar6_wg3_ch10 plot --ar6-data="R10" 2
+python3 ar6_wg3_ch10 plot --ar6-data="country" 2
+
+python3 ar6_wg3_ch10 plot --ar6-data="world" 4
+python3 ar6_wg3_ch10 plot --ar6-data="R5" 4
+python3 ar6_wg3_ch10 plot --ar6-data="R10" 4
+python3 ar6_wg3_ch10 plot --ar6-data="country" 4
+
+python3 ar6_wg3_ch10 plot --ar6-data="world" 5
+python3 ar6_wg3_ch10 plot --ar6-data="R5" 5
+python3 ar6_wg3_ch10 plot --ar6-data="R10" 5
+python3 ar6_wg3_ch10 plot --ar6-data="country" 5
+
+python3 ar6_wg3_ch10 plot --ar6-data="world" 6
+python3 ar6_wg3_ch10 plot --ar6-data="R5" 6
+python3 ar6_wg3_ch10 plot --ar6-data="R10" 6
+python3 ar6_wg3_ch10 plot --ar6-data="country" 6
