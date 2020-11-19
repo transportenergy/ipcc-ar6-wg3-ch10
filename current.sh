aff6dfd419227c42c341ad8bcc3fe2f9d50fdc85
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-11-19 11:28:54 +0100

    Simplify current.sh script

--- current.sh
+++ current.sh
@@ -3,39 +3,19 @@ set -x
 
 PLOT="python3 ar6_wg3_ch10 plot"
 
-$PLOT --ar6-data="world" 1
-$PLOT --ar6-data="R5" 1
-$PLOT --ar6-data="R10" 1
-$PLOT --ar6-data="country" 1
+for fig in 1 2 4 5 6 7; do
+for ss in world R5 R10 country; do
+for recat in "" "--recategorize=A" "--recategorize=B"; do
 
-$PLOT --ar6-data="world" 2
-$PLOT --ar6-data="R5" 2
-$PLOT --ar6-data="R10" 2
-$PLOT --ar6-data="country" 2
+$PLOT --ar6-data="$ss" $recat $fig
 
-$PLOT --ar6-data="world" 4
-$PLOT --ar6-data="R5" 4
-$PLOT --ar6-data="R10" 4
-$PLOT --ar6-data="country" 4
-
-$PLOT --ar6-data="world" 5
-$PLOT --ar6-data="R5" 5
-$PLOT --ar6-data="R10" 5
-$PLOT --ar6-data="country" 5
-
-$PLOT --ar6-data="world" 6
-$PLOT --ar6-data="R5" 6
-$PLOT --ar6-data="R10" 6
-$PLOT --ar6-data="country" 6
-
-# # Variants for M.Craig
-# for ss in world R5 R10 country
-# do
-#   for recat in A B
-#   do
-#     for bw in 5 8 9
-#     do
-#       $PLOT --ar6-data="$ss" --recategorize="$recat" --bandwidth="$bw" 6
-#     done
-#   done
+# # fig_6 variants for M.Craig. --bandwidth=9 is default, so don't repeat
+# for bw in 5 8; do
+#
+# $PLOT --ar6-data="$ss" $recat --bandwidth="$bw" 6
+#
 # done
+
+done
+done
+done
