1fc1232bda3e032a5be64ec5f38f40b96e4e6dad
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-12-02 22:06:33 +0100

    Add comments to current.sh

--- current.sh
+++ current.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -x
 
+# Add --skip-cache here when the initial data-loading code changes
 PLOT="python3 ar6_wg3_ch10 plot"
 
 for fig in 1 2 4 5 6 7; do
 for ss in world R5 R10 country; do
 for recat in "" "--recategorize=A" "--recategorize=B"; do
 
+# Add --per-capita here, e.g. for fig_2
 $PLOT --ar6-data="$ss" $recat $fig
 
 # # fig_6 variants for M.Craig. --bandwidth=9 is default, so don't repeat
